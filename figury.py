b824a9d4e8da65deac6e99436391b9d071a9748d
SzymonTom <161208826+SzymonTom@users.noreply.github.com>
2025-05-09 15:28:44 +0200

    Update figury.py

--- figury.py
+++ figury.py
@@ -1,7 +1,7 @@
 from ursina import *
 
 class ChessPiece(Entity):
-    def __init__(self, piece_type: str, color: str, position: str):
+    def __init__(self, piece_type: str, kolor: str, position: str):
         """
         Inicjalizuje figurę szachową.
 
@@ -22,15 +22,24 @@ class ChessPiece(Entity):
             'pawn': 'models/pionek.obj',
         }
         world_position = self._board_to_world(position)
-        obrot=180 if color == 'white' else 0
+        obrot=180 if kolor == 'white' else 0
         super().__init__(
             model=self.piece_types[piece_type],
-            texture=self.piece_colors[color],
+            texture=self.piece_colors[kolor],
             position=world_position,
             scale=1.0,
             rotation=(0, obrot, 0),
+            collider=self.piece_types[piece_type],
             #shader=basic_lighting_shader,
             #origin_y=-0.5  # Ustawienie na "podłodze"
+        )
+        self.highlight = Entity(
+            parent=self,
+            model=self.piece_types[piece_type],
+            color=color.white,
+            scale=1,
+            alpha=0,
+            always_on_top=True
         )
     def _board_to_world(self,position: str) -> Vec3:
         """
@@ -41,22 +50,30 @@ class ChessPiece(Entity):
         x = -21+6*(ord(position[0]) - ord('a'))
         y = -21+6*(int(position[1]) - 1)
         return Vec3(x, 0, y)  # Centruje planszę na (0,0,0)
+    def highlight_piece(self):
+        """
+        Sprawdza, czy myszka znajduje się nad figurą, i podświetla ją.
+        """
+        if mouse.hovered_entity == self:
+            self.highlight.alpha = 0.5  # Show highlight
+        else:
+            self.highlight.alpha = 0  # Hide highlight
 class Rook(ChessPiece):
-    def __init__(self, color: str, position: str):
-        super().__init__('rook', color, position)
+    def __init__(self, kolor: str, position: str):
+        super().__init__('rook', kolor, position)
         #self.has_moved = False  # Flaga do sprawdzania, czy wieża się poruszała
 class Knight(ChessPiece):
-    def __init__(self, color: str, position: str):
-        super().__init__('knight', color, position)
+    def __init__(self, kolor: str, position: str):
+        super().__init__('knight', kolor, position)
 class Bishop(ChessPiece):   
-    def __init__(self, color: str, position: str):
-        super().__init__('bishop', color, position)
+    def __init__(self, kolor: str, position: str):
+        super().__init__('bishop', kolor, position)
 class Queen(ChessPiece):
-    def __init__(self, color: str, position: str):
-        super().__init__('queen', color, position)
+    def __init__(self, kolor: str, position: str):
+        super().__init__('queen', kolor, position)
 class King(ChessPiece):
-    def __init__(self, color: str, position: str):
-        super().__init__('king', color, position)
+    def __init__(self, kolor: str, position: str):
+        super().__init__('king', kolor, position)
 class Pawn(ChessPiece):
-    def __init__(self, color: str, position: str):
-        super().__init__('pawn', color, position)
+    def __init__(self, kolor: str, position: str):
+        super().__init__('pawn', kolor, position)
