dcfb9fb48a40848ad42a3d35371bcdc8557bcbe0
SzymonTom <161208826+SzymonTom@users.noreply.github.com>
2025-05-09 14:50:25 +0200

    Dodano pliki

---
+++ board.py
@@ -0,0 +1,18 @@
+from ursina import *
+
+def load_board():
+    board_1 = Entity(
+    model='jasna_rama.obj',
+    texture='Sapeli.jpg',
+    scale=1
+    )
+    board_2 = Entity(
+    model='jasna_kratka.obj',
+    texture='Sapeli.jpg',
+    scale=1
+    )
+    board_3 = Entity(
+    model='ciemna_rama.obj',
+    texture='Dark_Mahony.jpg',
+    scale=1
+    )
---
+++ figury.py
@@ -0,0 +1,62 @@
+from ursina import *
+
+class ChessPiece(Entity):
+    def __init__(self, piece_type: str, color: str, position: str):
+        """
+        Inicjalizuje figurę szachową.
+
+        :param piece_type: Typ figury ('rook', 'knight', 'bishop', 'queen', 'king', 'pawn')
+        :param color: Kolor figury ('white' lub 'black')
+        :param position: Pozycja na planszy (np. 'e4')
+        """
+        self.piece_colors = {
+            'white': 'Sapeli.jpg',
+            'black': 'Dark_Mahony.jpg'
+        }
+        self.piece_types = {
+            'rook': 'models/wieza.obj',
+            'knight': 'models/kon.obj',
+            'bishop': 'models/goniec.obj',
+            'queen': 'models/krolowa.obj',
+            'king': 'models/krol.obj',
+            'pawn': 'models/pionek.obj',
+        }
+        world_position = self._board_to_world(position)
+        obrot=180 if color == 'white' else 0
+        super().__init__(
+            model=self.piece_types[piece_type],
+            texture=self.piece_colors[color],
+            position=world_position,
+            scale=1.0,
+            rotation=(0, obrot, 0),
+            #shader=basic_lighting_shader,
+            #origin_y=-0.5  # Ustawienie na "podłodze"
+        )
+    def _board_to_world(self,position: str) -> Vec3:
+        """
+        Konwertuje pozycję szachową (np. 'e4') na współrzędne 3D.
+        """
+        if position[0] not in 'abcdefgh' or position[1] not in '12345678':
+            raise ValueError("Invalid chess position")
+        x = -21+6*(ord(position[0]) - ord('a'))
+        y = -21+6*(int(position[1]) - 1)
+        return Vec3(x, 0, y)  # Centruje planszę na (0,0,0)
+class Rook(ChessPiece):
+    def __init__(self, color: str, position: str):
+        super().__init__('rook', color, position)
+        #self.has_moved = False  # Flaga do sprawdzania, czy wieża się poruszała
+class Knight(ChessPiece):
+    def __init__(self, color: str, position: str):
+        super().__init__('knight', color, position)
+class Bishop(ChessPiece):   
+    def __init__(self, color: str, position: str):
+        super().__init__('bishop', color, position)
+class Queen(ChessPiece):
+    def __init__(self, color: str, position: str):
+        super().__init__('queen', color, position)
+class King(ChessPiece):
+    def __init__(self, color: str, position: str):
+        super().__init__('king', color, position)
+class Pawn(ChessPiece):
+    def __init__(self, color: str, position: str):
+        super().__init__('pawn', color, position)
